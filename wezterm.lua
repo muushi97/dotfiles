4933d352d177f11f5f233c8d2a2dc2e4b9bb4836
muushi <moouushi@gmail.com>
2026-07-20 16:48:33 +0900

    update wezterm config: VGA retro theme and font setup

--- wezterm.lua
+++ wezterm.lua
@@ -2,28 +2,112 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- WSL
-config.default_domain = 'WSL:Ubuntu'
-config.automatically_reload_config = true
-config.use_ime = true
+config.font_dirs = { wezterm.config_dir .. '/fonts' }
 
--- Color scheme
-config.color_scheme = 'Catppuccin Mocha'
+----------------------------------------------------------------
+-- フォント: OSインストール不要で読み込む
+----------------------------------------------------------------
 
--- Tab bar
+-- Px437 IBM VGA8: 実機VGA BIOSの8x16ビットマップをそのまま復刻したフォント
+-- (int10h.org "The Ultimate Oldschool PC Font Pack" より, CC BY-SA 4.0)
+config.font = wezterm.font('Mx437 IBM VGA 8x16', { weight = 'Regular' })
+config.font_rules = {
+  {
+    intensity = 'Bold',
+    font = wezterm.font('Mx437 IBM VGA 8x16', { weight = 'Regular' }),
+  },
+}
+-- 合字(リガチャ)を無効化
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+-- ビットマップの角をにじませない
+config.freetype_load_target = 'Mono'
+config.freetype_render_target = 'Mono'
+-- フォント諸設定
+config.font_size = 12.0  -- dpi 96 のときに 16px
+config.line_height = 1.0
+config.cell_width = 1.0
+--config.front_end = "OpenGL"
+
+
+----------------------------------------------------------------
+-- 配色: 昔ながらのVGAテキストモード16色パレット
+----------------------------------------------------------------
+
+config.colors = {
+  foreground = '#AAAAAA', -- 標準テキスト(古いDOSプロンプトのライトグレー)
+  background = '#000000',
+  cursor_bg = '#AAAAAA',
+  cursor_fg = '#000000',
+  cursor_border = '#AAAAAA',
+  selection_fg = '#000000',
+  selection_bg = '#AAAAAA',
+ 
+  ansi = {
+    '#000000', -- black
+    '#AA0000', -- red
+    '#00AA00', -- green
+    '#AA5500', -- brown/yellow
+    '#0000AA', -- blue
+    '#AA00AA', -- magenta
+    '#00AAAA', -- cyan
+    '#AAAAAA', -- light gray
+  },
+  brights = {
+    '#555555', -- dark gray
+    '#FF5555', -- light red
+    '#55FF55', -- light green
+    '#FFFF55', -- yellow
+    '#5555FF', -- light blue
+    '#FF55FF', -- light magenta
+    '#55FFFF', -- light cyan
+    '#FFFFFF', -- white
+  },
+}
+
+
+----------------------------------------------------------------
+-- カーソル: 古い端末らしい点滅ブロックカーソル
+----------------------------------------------------------------
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_rate = 500
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+
+
+----------------------------------------------------------------
+-- ウィンドウまわり: 余計な装飾を削ぎ落とす
+----------------------------------------------------------------
+
+config.window_padding = {
+  left = 8,
+  right = 8,
+  top = 8,
+  bottom = 8,
+}
+config.window_background_opacity = 1.0
+config.enable_scroll_bar = true
+ 
+-- タブバーもレトロな見た目に
 config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = true
 
--- Font
---config.font = wezterm.font_with_fallback {
---  'FiraCode Nerd Font',
---  'Noto Sans CJK JP', -- 日本語フォールバック
---}
-config.font_size = 9.0
 
--- Window size
-config.initial_cols = 220
-config.initial_rows = 50
+----------------------------------------------------------------
+-- WSL まわりの設定
+----------------------------------------------------------------
+
+config.default_domain = 'WSL:Ubuntu'
+config.automatically_reload_config = true
+
+
+----------------------------------------------------------------
+-- ...
+----------------------------------------------------------------
+
+-- 初期ウィンドウサイズ (文字数)
+config.initial_cols = 80
+config.initial_rows = 25
 
 -- Scrollback
 config.scrollback_lines = 10000
@@ -31,7 +115,14 @@ config.scrollback_lines = 10000
 -- Mouse
 config.pane_focus_follows_mouse = true
 
--- Key bindings
+-- IME
+config.use_ime = true
+
+
+----------------------------------------------------------------
+-- キーバインド
+----------------------------------------------------------------
+
 config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
   -- ペイン分割
@@ -52,7 +143,11 @@ config.keys = {
   { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
 }
 
--- Status bar (右上に時刻を表示)
+
+----------------------------------------------------------------
+-- ステータスバー
+----------------------------------------------------------------
+
 wezterm.on('update-right-status', function(window, _pane)
   local date = wezterm.strftime '%Y-%m-%d %H:%M'
   window:set_right_status(wezterm.format {
